baf3a8652be5094689195212a1fabef5e3bd43c7
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-21 11:58:20 -0800

    Add swap test description

--- test/swap-test.js
+++ test/swap-test.js
@@ -1,2 +1,10 @@
 const assert = require('chai').assert;
 const swapNumbers = require('../swap');
+
+describe('swap functionality', () => {
+  context('swap function', () => {
+    it('can swap two unsorted numbers', () => {
+
+    });
+  });
+});
