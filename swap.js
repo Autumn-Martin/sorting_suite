61f2d183dabaa7c78f5fcaa3d17e1aacb27407e7
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-21 12:44:43 -0800

    Learning about exports

--- swap.js
+++ swap.js
@@ -1,10 +1,11 @@
-module.exports = swap
+var exports = module.exports = {};
 
+exports.unsortedArray = [5, 0, 1, 3, 4, 2]
 const unsortedArray = [5, 0, 1, 3, 4, 2]
 let currentIndex = 0
 let nextIndex = currentIndex + 1
 
-function swap(unsortedArray, currentIndex, nextIndex) {
+exports.swap = function(unsortedArray, currentIndex, nextIndex) {
   if (unsortedArray[currentIndex] > unsortedArray[nextIndex]) {
     var briefHolder = unsortedArray[nextIndex];
 
--- test/swap-test.js
+++ test/swap-test.js
@@ -4,8 +4,8 @@ const swapNumbers = require('../swap');
 describe('swap functionality', () => {
   context('swap function', () => {
     it('can swap two unsorted numbers', () => {
-      const unsorted_array = [5, 0, 1, 3, 4, 2];
-      assert.deepEqual(swapNumbers(unsorted_array, 0, 1), [0, 5, 1, 3, 4, 2])
+      // const unsortedArray = [5, 0, 1, 3, 4, 2];
+      assert.deepEqual(swapNumbers.swap(swapNumbers.unsortedArray, 0, 1), [0, 5, 1, 3, 4, 2])
     });
   });
 });
