3dd78c2646ad865fbb0cee52e86003371affad4a
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-22 09:21:26 -0800

    Remove unnec code from swap file

--- swap.js
+++ swap.js
@@ -1,9 +1,5 @@
 module.exports = swap
 
-const unsortedArray = [5, 0, 1, 3, 4, 2]
-let currentIndex = 0
-let nextIndex = currentIndex + 1
-
 function swap(unsortedArray, currentIndex, nextIndex) {
     var briefHolder = unsortedArray[nextIndex];
 
