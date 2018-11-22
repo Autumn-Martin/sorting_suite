09fe3d1afec5f5a60dc26f11e88c30b363fa9dda
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-21 22:59:12 -0800

    Add swap function to bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,6 +1,14 @@
 module.exports = bubbleSort
-const swapNumbers = require('./swap')
 
-function bubbleSort() {
+const swapNumbers = require('./swap');
+const unsortedArray = [5, 0, 1, 3, 4, 2];
+const sortedArray = bubbleSort(unsortedArray);
 
+function bubbleSort(unsortedArray) {
+  let currentIndex = 0;
+  let nextIndex = currentIndex + 1;
+  swapNumbers(unsortedArray, currentIndex, nextIndex)
+  return unsortedArray
 }
+
+console.log(bubbleSort(unsortedArray))
