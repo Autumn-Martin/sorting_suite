1cd029c2fab4a5ceca771df2dc9b77a6cbdd6c37
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-22 11:41:10 -0800

    Add assertion for different arrays

--- test/bubble-sort-test.js
+++ test/bubble-sort-test.js
@@ -4,8 +4,14 @@ const bubbleSort = require('../bubble-sort');
 describe('bubble sort functionality', () => {
   context('bubbleSort function', () => {
     it('can sort an unsorted array', () => {
-      const unsortedArray = [5, 0, 1, 3, 4, 2];
-      assert.deepEqual(bubbleSort(unsortedArray), [0,1,2,3,4,5])
+      let unsortedArray = [5, 0, 1, 3, 4, 2];
+      assert.deepEqual(bubbleSort(unsortedArray), [0, 1, 2, 3, 4, 5])
+
+      unsortedArray = [5, 4, 3, 2, 1, 0]
+      assert.deepEqual(bubbleSort(unsortedArray), [0, 1, 2, 3, 4, 5])
+
+      unsortedArray = [42, 32, 70, 0, -9]
+      assert.deepEqual(bubbleSort(unsortedArray), [-9, 0, 32, 42, 70])
     });
   });
 });
