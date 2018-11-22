7d92fabae0e481ccf88b1e3af8655e7d297f923e
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-21 23:34:12 -0800

    Extract conditional logic

--- swap.js
+++ swap.js
@@ -5,12 +5,10 @@ let currentIndex = 0
 let nextIndex = currentIndex + 1
 
 function swap(unsortedArray, currentIndex, nextIndex) {
-  if (unsortedArray[currentIndex] > unsortedArray[nextIndex]) {
     var briefHolder = unsortedArray[nextIndex];
 
     unsortedArray[nextIndex] = unsortedArray[currentIndex];
     unsortedArray[currentIndex] = briefHolder;
 
     return unsortedArray;
-  }
 }
