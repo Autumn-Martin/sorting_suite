23b996e9e74a6b9797396336db93908be9ed0a3a
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-22 08:30:26 -0800

    Add while not sorted loop & sort full array

--- bubble-sort.js
+++ bubble-sort.js
@@ -5,16 +5,28 @@ const unsortedArray = [5, 0, 1, 3, 4, 2];
 const sortedArray = bubbleSort(unsortedArray);
 
 function bubbleSort(unsortedArray) {
-  const lastIndexToSort = unsortedArray.length - 1
+  let sorted = false;
 
-  for(let currentIndex = 0; currentIndex < lastIndexToSort; currentIndex++) {
-    let nextIndex = currentIndex + 1;
+  while(!sorted) {
+    let lastIndexToSort = unsortedArray.length - 1;
 
-    if (unsortedArray[currentIndex] > unsortedArray[nextIndex]) {
-      swapNumbers(unsortedArray, currentIndex, nextIndex)
+    sorted = true;
+    for(let currentIndex = 0; currentIndex < lastIndexToSort; currentIndex++) {
+      let nextIndex = currentIndex + 1;
+
+      if (unsortedArray[currentIndex] > unsortedArray[nextIndex]) {
+        swapNumbers(unsortedArray, currentIndex, nextIndex)
+        console.log(unsortedArray)
+        sorted = false;
+      }
+      console.log(currentIndex)
     }
+    var runNum = typeof runNum == "undefined" ? runNum = 1 : runNum = runNum;
+    runNum += 1
+    console.log(`run ${runNum}`)
   }
+  console.log("break")
   return unsortedArray
 }
 
-console.log(bubbleSort(unsortedArray))
+console.log(sortedArray)
