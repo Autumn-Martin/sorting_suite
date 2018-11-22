30d87cca7d8de0e87aa8f029dcbf1efcdd78a23a
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-22 15:55:56 -0800

    Experiment with testing console output

--- test/swap-test.js
+++ test/swap-test.js
@@ -2,6 +2,9 @@ const chai = require('chai')
 const assert = chai.assert;
 const expect = chai.expect;
 const swapNumbers = require('../swap');
+var stdout = require("test-console").stdout;
+
+var output = stdout.inspectSync(() => {swapNumbers([5, 0, 1, 3, 4, 2], 0, 1)});
 
 describe('swap functionality', () => {
   context('swap function', () => {
@@ -11,6 +14,10 @@ describe('swap functionality', () => {
     });
     it('is a function', () => {
       expect(swapNumbers).to.be.a('Function');
+    })
+    it('says hello', () => {
+
+      assert.deepEqual(output, [ "hi\n"]);
     })
   });
 });
